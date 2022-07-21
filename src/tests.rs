cb7ba4480ec71c68f59d07f0ac641483902f0df3
janvanessen <jan.vanessen@babelo.de>
2022-07-21 18:40:05 +0200

    Fix test

--- src/tests.rs
+++ src/tests.rs
@@ -3,7 +3,7 @@ use rocket::local::Client;
 
 #[test]
 fn hello_world() {
-    let rocket = rocket::ignite().mount("/", routes![super::hello]);
+    let rocket = rocket::ignite().mount("/", routes![super::home]);
     let client = Client::new(rocket).unwrap();
     let mut response = client.get("/").dispatch();
     assert_eq!(response.body_string(), Some("Hello world!".into()));
